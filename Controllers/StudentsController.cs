6e6110b91f357c0de0dc26e713435c2f5c35bd6b
Selim <selimshaikh2050@gmail.com>
2025-11-07 01:30:26 +0600

    Complete Ticker/

--- Controllers/StudentsController.cs
+++ Controllers/StudentsController.cs
@@ -1,11 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DNN.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DNN.Controllers
 {
     public class StudentsController : Controller
     {
+        private readonly DNNDbContext _context;
+
+        public StudentsController(DNNDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+            var activeNotices = _context.Notices
+                .Where(n => n.IsActive && (n.ExpireDate == null || n.ExpireDate > DateTime.Now))
+                .OrderByDescending(n => n.EntryDate)
+                .Take(5)
+                .ToList();
+
+            ViewBag.ActiveNotices = activeNotices;
+
             return View();
         }
     }
